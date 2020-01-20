72284720960ba59f243e07e980781091a7152113
Jay Abi-Saad <jay@Jays-MacBook-Pro.local>
2020-01-19 19:38:57 -0500

    Added continuous input, however, new feature only allows single word input in mysh - to be fixed in further commit.

--- shell.c
+++ shell.c
@@ -4,13 +4,13 @@
 #include "interpreter.h"
 #include "shellmemory.h"
 
-char** parse(char userin[]) {
+char** parse(char userInput[]) {
     char ** parsedUserInput = malloc(100 * sizeof(char*));
     for (int i = 0 ; i < 100; ++i) {
         parsedUserInput[i] = malloc(100 * sizeof(char));
     }
     int i = 0;
-    char* token = strtok(userin, " ");
+    char* token = strtok(userInput, " ");
     while (token != NULL) {
         parsedUserInput[i] = token;
         token = strtok(NULL, " ");
@@ -19,12 +19,15 @@ char** parse(char userin[]) {
     return parsedUserInput;
 }
 
-int main(int argc, char * argv[]) {
-    char userinput[50];
-    printf("Welcome to the JayJay shell!\n");
-    printf("Version 1.0 Created January 2020\n");
-    printf("$ ");
-    scanf("%[^\n]s", userinput);
-    char** okok = parse(userinput);
-    interpret(okok);
+int main() {
+    char userInput[50];
+    do {
+        printf("Welcome to the JayJay shell!\n");
+        printf("Version 1.0 Created January 2020\n");
+        printf("$ ");
+        scanf("%s", userInput);
+        interpret(parse(userInput));
+	}
+	while(strcmp(parse(userInput)[0],"quit")!=0);
+	printf("Bye!\n");
 }
